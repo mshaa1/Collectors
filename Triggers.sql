0c224cab851d0563496396040aab862e983cf421
Loruski <lorusso116@gmail.com>
2023-05-18 11:26:22 +0200

    Aggiunti commenti a Procedures.sql, Aggiunto Triggers.sql che conterrà i triggers del DB, al suo interno vi sono i 3 triggers che aggiornano il numero di duplicati alla insert, delete, o update di una entry in comprende_dischi

---
+++ Triggers.sql
@@ -0,0 +1,22 @@
+USE collectors;
+DELIMITER $
+
+/* Sezione Update duplicati */
+
+CREATE TRIGGER Update_Duplicati_On_Insert_Comprende_Dischi AFTER INSERT ON Comprende_Dischi FOR EACH ROW
+    BEGIN
+        CALL Aggiornamento_Duplicati(NEW.ID_collezione, NEW.ID_Disco, 'INSERT');
+    END$
+
+CREATE TRIGGER Update_Duplicati_On_Delete_Comprende_Dischi AFTER DELETE ON Comprende_Dischi FOR EACH ROW
+    BEGIN
+        CALL Aggiornamento_Duplicati(OLD.ID_collezione, OLD.ID_Disco, 'DELETE');
+    END$
+
+CREATE TRIGGER Update_Duplicati_On_Update_Comprende_Dischi AFTER UPDATE ON Comprende_Dischi FOR EACH ROW
+    BEGIN
+        IF OLD.ID_collezione = NEW.ID_collezione AND OLD.ID_disco <> NEW.ID_disco THEN
+        CALL Aggiornamento_Duplicati(OLD.ID_collezione, OLD.ID_Disco, 'DELETE');
+        CALL Aggiornamento_Duplicati(NEW.ID_collezione, NEW.ID_Disco, 'INSERT');
+            END IF;
+    END$
