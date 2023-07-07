ecb3128a4f0c18f18d11a3ad92c564fc464950e1
Server-Booster <dicrescemanuel@gmail.com>
2023-07-07 15:33:52 +0200

    Iniziato popolamento classi del domain model.

--- collectors/Collectors/src/main/java/it/collectors/model/Immagine.java
+++ collectors/Collectors/src/main/java/it/collectors/model/Immagine.java
@@ -1,4 +1,46 @@
 package it.collectors.model;
 
+import java.io.InputStream;
+
 public class Immagine {
+
+    private final Integer id;
+
+    private final String filePath;
+
+    private final String didascalia;
+
+    private final InputStream content;
+
+    private final byte[] cache;
+
+
+    public Immagine(Integer id, String filePath, String descrizione, InputStream content, byte[] cache) {
+        this.id = id;
+        this.filePath = filePath;
+        this.didascalia = descrizione;
+        this.content = content;
+        this.cache = cache;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getDidascalia() {
+        return didascalia;
+    }
+
+    public InputStream getContent() {
+        return content;
+    }
+
+    public byte[] getCache() {
+        return cache;
+    }
+
 }
--- collectors/Collectors/src/main/java/it/collectors/model/Traccia.java
+++ collectors/Collectors/src/main/java/it/collectors/model/Traccia.java
@@ -1,4 +1,32 @@
 package it.collectors.model;
 
+import java.sql.Time;
+
 public class Traccia {
+
+    private final Integer id;
+
+    private final String titolo;
+
+    private final Time durata;
+
+
+    public Traccia(Integer id, String titolo, Time durata) {
+        this.id = id;
+        this.titolo = titolo;
+        this.durata = durata;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public Time getDurata() {
+        return durata;
+    }
+
 }
